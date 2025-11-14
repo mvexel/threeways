e432546d38837b1da5972a6ea58c1d0639eb98fe
Martijn van Exel <m@rtijn.org>
2025-11-13 21:07:43 -0700

    cascade it

--- sql/001_init.sql
+++ sql/001_init.sql
@@ -5,8 +5,7 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS hstore;
 
 -- H3 requires the custom docker/postgis image which installs the extension via PGXN.
-CREATE EXTENSION IF NOT EXISTS h3;
-CREATE EXTENSION IF NOT EXISTS h3_postgis;
+CREATE EXTENSION IF NOT EXISTS h3_postgis CASCADE;
 
 -- Create OSM schema (osm2pgsql will use this)
 CREATE SCHEMA IF NOT EXISTS osm;
