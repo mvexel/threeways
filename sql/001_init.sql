e5a1dfdfe0121d76256f4ae842300310a47df192
Martijn van Exel <m@rtijn.org>
2025-11-13 20:54:45 -0700

    add h3 build

--- sql/001_init.sql
+++ sql/001_init.sql
@@ -4,10 +4,9 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS hstore;
 
--- H3 is optional and requires pre-installation via PGXN or custom build.
--- Uncomment once the extension is installed in the Postgres image.
--- CREATE EXTENSION IF NOT EXISTS h3;
--- CREATE EXTENSION IF NOT EXISTS h3_postgis;
+-- H3 requires the custom docker/postgis image which installs the extension via PGXN.
+CREATE EXTENSION IF NOT EXISTS h3;
+CREATE EXTENSION IF NOT EXISTS h3_postgis;
 
 -- Create OSM schema (osm2pgsql will use this)
 CREATE SCHEMA IF NOT EXISTS osm;
